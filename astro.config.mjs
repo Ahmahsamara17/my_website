cc3d7aad4e8c19c22418e77cd72efaf5408b69a6
Ahmad Samara <ahmadsamara177@gmail.com>
2025-10-22 02:11:40 -0700

    Feat: github pages set up

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,8 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://ahmahsamara17.github.io/my_website/",
+  base: "/my_website/",
   integrations: [],
 
   vite: {
